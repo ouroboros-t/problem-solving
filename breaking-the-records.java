304f7d4bd6dde45c2a3132d34b24a8280d4643f3
ouroboros-t <87835227+ouroboros-t@users.noreply.github.com>
2022-01-10 15:21:42 -0500

    Create breaking-the-reacords.java
    via hackerrank problem

---
+++ breaking-the-records.java
@@ -0,0 +1,113 @@
+/*
+
+Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. 
+She tabulates the number of times she breaks her season record for most points and least points in a game. 
+Points scored in the first game establish her record for the season, and she begins counting from there.
+
+Given the scores for a season, determine the number of times Maria breaks her records for most and least points scored during the season.
+
+Function Description
+
+Complete the breakingRecords function in the editor below.
+
+breakingRecords has the following parameter(s):
+
+int scores[n]: points scored per game
+Returns
+
+int[2]: An array with the numbers of times she broke her records. Index  is for breaking most points records, and index  is for breaking least points records.
+
+
+Sample Input 0
+9
+10 5 20 20 4 5 2 25 1
+
+Sample Output 0
+2 4
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+    // Write your code here
+    int minScore = scores.get(0); //scores.get(0) = 10;
+    //System.out.println(minScore);
+    int maxScore =scores.get(0);
+    //System.out.println(maxScore);
+    int minCount = 0;
+    int maxCount = 0;
+    List<Integer> recordCount  = new ArrayList<Integer>();
+    
+    for(Integer score : scores){
+        if(score < minScore){
+            minScore = score;
+            minCount++;
+            //System.out.println(minScore);
+        }
+        if(score > maxScore){
+            maxScore = score;
+            maxCount++;
+        }
+    }
+    recordCount.add(maxCount);
+    recordCount.add(minCount);
+    return recordCount;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
